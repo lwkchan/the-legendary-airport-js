3575fadb5f63fb0dd0c2545978a30fa122865883
Laura Chan <laurachan205@gmail.com>
2018-02-21 12:27:45 +0000

    First commit

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -2,28 +2,35 @@ describe ('Airport', function() {
 
   var airport;
 
-  it("lands airplanes", function() {
+  beforeEach(function() {
     airport = new Airport();
+  });
+
+  it("lands airplanes", function() {
     airport.land('plane');
     airport.land('another plane');
     expect(airport.hangar).toEqual(['plane', 'another plane']);
   });
 
   it("removes airplanes from the airport", function() {
-    airport = new Airport();
     airport.land('plane');
     airport.land('another plane');
-    spyOn(Math, 'random').and.returnValue(10)
+    spyOn(Math, 'random').and.returnValue(0.9);
     airport.takeoff('plane')
     expect(airport.hangar).toEqual(['another plane']);
   });
 
   it("doesn't allow planes to take off if stormy", function() {
-    airport = new Airport();
     airport.land('plane');
-    spyOn(Math, 'random').and.returnValue(0.1)
+    spyOn(Math, 'random').and.returnValue(0.1);
     airport.takeoff('plane');
     expect(airport.hangar).toEqual(['plane']);
+  });
+
+  it("doesn't allow planes to land if it's stormy", function() {
+    spyOn(Math, 'random').and.returnValue(0.1);
+    airport.land('plane');
+    expect(airport.hanger).not.toEqual(['plane']);
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,27 +1,28 @@
 var Airport = function(){
   this.hangar = [];
+  this.isStormy = function(){
+    var chance = Math.floor((Math.random() * 10) + 1);
+    if (chance < 3) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 };
 
 Airport.prototype.land = function(plane) {
-  this.hangar.push(plane);
-};
-
-Airport.prototype.isStormy = function () {
-  var chance = Math.floor((Math.random() * 10) + 1);
-  if (chance < 3) {
-    return true;
-  } else {
-    return false;
-  }
+  if (this.isStormy===true) {
+    console.log("NO! NO LAND STORMY");
+  } else {this.hangar.push(plane);};
 };
 
 Airport.prototype.takeoff = function(plane) {
-  if (this.isStormy()===true) {
-    console.log("NO!");
+  if (this.isStormy===true) {
+    console.log("NO! NO TAKEOFF STORMY");
   } else {
       var index = this.hangar.indexOf(plane);
       if (index === -1) {
-        console.log("NO!");
+        console.log("NO! NO PLANE EXIST");
       } else {
         this.hangar.splice(index, 1);
       }
@@ -29,5 +30,3 @@ Airport.prototype.takeoff = function(plane) {
   };
 
 var airport = new Airport();
-
-airport.land('a plane');
