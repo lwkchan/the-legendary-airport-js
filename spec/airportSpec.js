7f688865059e9ef038774fbbea4b6a1afd2590f5
Laura Chan <laurachan205@gmail.com>
2018-02-21 12:44:51 +0000

    Relocated isStormy fuction from prototype to constructor function

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -15,22 +15,19 @@ describe ('Airport', function() {
   it("removes airplanes from the airport", function() {
     airport.land('plane');
     airport.land('another plane');
-    spyOn(Math, 'random').and.returnValue(0.9);
     airport.takeoff('plane')
     expect(airport.hangar).toEqual(['another plane']);
   });
 
   it("doesn't allow planes to take off if stormy", function() {
     airport.land('plane');
-    spyOn(Math, 'random').and.returnValue(0.1);
     airport.takeoff('plane');
     expect(airport.hangar).toEqual(['plane']);
   });
 
   it("doesn't allow planes to land if it's stormy", function() {
-    spyOn(Math, 'random').and.returnValue(0.1);
     airport.land('plane');
-    expect(airport.hanger).not.toEqual(['plane']);
+    expect(airport.hangar).not.toEqual(['plane']);
   });
 
 });
